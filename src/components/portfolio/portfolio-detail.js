c57b07864b0c0dd44431a541bbe43e05548c887b
braydenmackay <braydenmackay1@gmail.com>
2019-10-11 16:30:05 -0600

    first commit

---
+++ src/components/portfolio/portfolio-detail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function (props) {
+    return (
+        <div>
+        <h2>Portfolio Detail for {props.match.params.slug}</h2>
+            
+         
+        </div>
+        
+    )
+}
