6661934099889b52a07f1c9a599619ee26b23f59
braydenmackay <braydenmackay1@gmail.com>
2019-10-23 13:14:32 -0600

    styling to about and contact page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react"
+import profilePicture from "../../../static/assets/images/auth/login.jpg"
 
-export default function () {
-    return (
-        <div>
-            About
-        </div>
-    )
+export default function() {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + profilePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+        My name is Brayden. I am awesome. I am a software developer. Hire me and
+        your company will be awesome too.
+      </div>
+    </div>
+  )
 }
